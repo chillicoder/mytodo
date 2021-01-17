431f218a714fa962a85c39dbb96c535d7c56c370
Martin Trejo Chavez <chillicoder@gmail.com>
2021-01-17 13:08:50 -0600

    Agregando capa UI

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,7 +1,58 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate seed;
+use seed::prelude::*;
+use seed::{fetch, Request};
+use futures::Future;
+
+use mytodo::{JsonApiResponse, Task};
+
+#[derive(Clone, Debug)]
+enum Direction {
+    Coming,
+    Going,
+}
+
+struct Model {
+    direction: Direction,
+}
+
+#[derive(Clone, Debug)]
+enum Msg {
+    FetchedTasks(fetch::ResponseDataResult<JsonApiResponse>),
+}
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
     }
 }
+
+fn view(model: &Model) -> impl View<Msg> {
+    let greeting = match model.direction {
+        Direction::Coming => "Hello, World!",
+        Direction::Going => "¡Hasta la vista!",
+    };
+    h1![
+        class! {"heading"},
+        style!["height" => "100vh",
+        "width" => "100vw",
+        ],
+        { greeting },
+    ]
+}
+
+fn fetch_drills() -> impl Future<Item = Msg, Error = Msg> {
+    Request::new("http://localhost:8000/tasks/").fetch_json_data(Msg::FetchedTasks)
+}
+
+fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Model {
+    orders.perform_cmd(fetch_drills());
+    Model {
+        direction: Direction::Coming,
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    seed::App::build(init, update, view).finish().run();
+}
+
