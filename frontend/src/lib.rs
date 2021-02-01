234da2cefeef09370ce78aef3eca21985249c3c3
Martin Trejo Chavez <chillicoder@gmail.com>
2021-02-01 15:12:44 -0600

    Agrega capa UI con WebAssembly
      Actualiza versiones de crates en Cargo.toml
      Consulta lista de todo's desde el backend

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,14 +1,10 @@
+#[macro_use]
+extern crate seed;
 use futures::Future;
 use seed::{fetch, Request};
 use seed::{prelude::*, *};
 
 use mytodo::{JsonApiResponse, Task};
-
-#[derive(Clone, Debug)]
-enum Direction {
-    Coming,
-    Going,
-}
 
 struct Model {
     tasks: Vec<Task>,
@@ -20,35 +16,39 @@ enum Msg {
 }
 
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
-    match msg {}
+    match msg {
+        Msg::FetchedTasks(Ok(mut result)) => {
+            model.tasks.clear();
+            model.tasks.append(&mut result.data);
+        }
+        Msg::FetchedTasks(Err(reason)) => {
+            log!(format!("Error fetching: {:?}", reason));
+        }
+    }
 }
 
 fn view(model: &Model) -> impl View<Msg> {
-    let greeting = match model.direction {
-        Direction::Coming => "Hello, World!",
-        Direction::Going => "¡Hasta la vista!",
-    };
-    h1![
-        class! {"heading"},
-        style!["height" => "100vh",
-        "width" => "100vw",
-        ],
-        { greeting },
-    ]
+    let tasks: Vec<Node<Msg>> = model
+        .tasks
+        .iter()
+        .map(|t| li![{ t.title.clone() }])
+        .collect();
+
+    h1![{ "Tasks" }, ul![tasks,],]
 }
 
-fn fetch_drills() -> impl Future<Item = Msg, Error = Msg> {
+fn fetch_drills() -> impl Future<Output = Result<Msg, Msg>> {
     Request::new("http://localhost:8000/tasks/").fetch_json_data(Msg::FetchedTasks)
 }
 
-fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Model {
+fn init(_url: Url, orders: &mut impl Orders<Msg>) -> AfterMount<Model> {
     orders.perform_cmd(fetch_drills());
-    Model {
-        direction: Direction::Coming,
-    }
+    AfterMount::new(Model { tasks: vec![] })
 }
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    seed::App::build(init, update, view).finish().run();
+    seed::App::builder(update, view)
+        .after_mount(init)
+        .build_and_start();
 }
