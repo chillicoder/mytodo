ffecb29d1784bdf3f9b16b094a721a5e5a6c6b78
Martin Trejo Chavez <chillicoder@gmail.com>
2021-02-01 15:15:00 -0600

    Cambia backend para ser consumido desde el frontend
      Reestructura archivo backend.rs
      Agrega soporte a CORS
      Devuelve respuestas API

--- backend/src/bin/backend.rs
+++ backend/src/bin/backend.rs
@@ -1,11 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use backend::db::models::Task;
-use backend::db::query_task;
-use rocket_contrib::databases::diesel;
-use rocket_contrib::json::Json;
-use serde::Serialize;
-
 #[macro_use]
 extern crate rocket;
 extern crate serde;
@@ -13,28 +7,49 @@ extern crate serde;
 #[macro_use]
 extern crate rocket_contrib;
 
+use rocket_contrib::json::Json;
+
+use rocket_cors::{ AllowedHeaders, AllowedOrigins, Error};
+
+use backend::db::query_task;
+use mytodo::JsonApiResponse;
+use mytodo::Task;
+use rocket_contrib::databases::diesel;
+use serde::Serialize;
+
 #[database("sqlite_db")]
 struct MyDatabase(diesel::SqliteConnection);
-
-#[derive(Serialize)]
-struct JsonApiResponse {
-    data: Vec<Task>,
-}
 
 #[get("/tasks")]
 fn tasks_get(conn: MyDatabase) -> Json<JsonApiResponse> {
     let mut response = JsonApiResponse { data: vec![] };
 
-    for task in query_task(&*conn) {
-        response.data.push(task);
+    for db_task in query_task(&conn) {
+        let api_task = mytodo::Task {
+            id: db_task.id,
+            title: db_task.title,
+        };
+        response.data.push(api_task);
     }
 
     Json(response)
 }
 
-fn main() {
+fn main() -> Result<(),Error> {
+    let allowed_origins = AllowedOrigins::all();
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }.to_cors()?;
+
     rocket::ignite()
         .attach(MyDatabase::fairing())
         .mount("/", routes![tasks_get])
+        .attach(cors)
         .launch();
+
+    Ok(())
 }
